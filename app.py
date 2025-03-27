34e99aea7b729aa7b53000e648a1de4641afd4fa
tyler <tylermyeo@gmail.com>
2025-03-27 16:05:40 +0200

    Added currency conversion

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import json
 from dotenv import load_dotenv
 import os
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
@@ -38,6 +39,29 @@ data = worksheet.get_all_records()
 df = pd.DataFrame(data)
 products = df["Product"].unique() # Extract list of unique products
 
+# Fetch latest currency conversion rates (base = USD)
+EXCHANGE_API_URL = "https://v6.exchangerate-api.com/v6/b03ae4404090b6a8b1281057/latest/USD"
+response = requests.get(EXCHANGE_API_URL)
+# st.write("Actual URL fetched from:", response.url)
+# st.write("Raw response content:", response.text)
+conversion_rates = response.json().get("conversion_rates", {})
+# st.write("Fetched exchange rates:", conversion_rates)
+
+def convert_to_usd(amount, currency):
+    try:
+        rate = conversion_rates.get(currency)
+        if rate:
+            # st.write("Looking for rate for:", currency)
+            return round(amount / rate, 2)
+            # st.write("Rate found:", rates.get(currency))
+        else:
+            return None
+    except Exception:
+        return None
+
+# Add a new column for converted USD prices
+df["Amount (USD)"] = df.apply(lambda row:convert_to_usd(row["Amount"], row["Currency"]),axis=1)
+
 # Streamlit UI
 st.title("Global Pricing")
 st.write("View the latest pricing of digital products from different regions.")
@@ -49,5 +73,6 @@ product_df["Timestamp"] = pd.to_datetime(product_df["Timestamp"])
 latest_df = product_df.sort_values("Timestamp").groupby("Region", as_index=False).last()
 
 # Show results
-st.write(f"Latest prices for: **{selected_product}**")
-st.dataframe(latest_df)
+st.write(f"Latest prices for **{selected_product}**")
+df_sorted = latest_df.sort_values(by="Amount (USD)")
+st.dataframe(df_sorted)
